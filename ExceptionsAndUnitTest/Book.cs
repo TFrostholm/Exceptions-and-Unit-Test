4e31f20bbd72a92ae40cff639eeb4475ccb327b6
Toke Frostholm Rasmussen <toke1011@edu.easj.dk>
2016-02-23 19:29:19 +0100

    Created methods with exceptions

---
+++ ExceptionsAndUnitTest/Book.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Security.AccessControl;
+
+namespace ExceptionsAndUnitTest
+{
+    public class Book
+    {
+        private string _title;
+        private int _year;
+
+
+        //Title must be at least 2 characters long
+        //Year must be between 1100 and 2015
+
+        public void SetTitle(string title)
+        {
+            if (title.Length < 2)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            _title = title;
+        }
+
+        public string GetTitle()
+        {
+            return _title;
+        }
+
+
+        public void SetYear(int year)
+        {
+            if (year < 1100 || year > 2015)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            _year = year;
+        }
+
+        public int GetYear()
+        {
+            return _year;
+        }
+    }
+}
--- ExceptionsAndUnitTest/Program.cs
+++ ExceptionsAndUnitTest/Program.cs
@@ -10,6 +10,32 @@ namespace ExceptionsAndUnitTest
     {
         static void Main(string[] args)
         {
+            Book b = new Book();
+
+            // try setting year below year 1100
+            try
+            {
+                b.SetYear(200);
+                Console.WriteLine("The book was written in {0}", b.GetYear());
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Invalid year: The database only holds records for books written between 1100 and 2015");
+            }
+
+            // Try setting title with less than 2 characters
+            try
+            {
+                b.SetTitle("a");
+                Console.WriteLine("The title of the book is {0}", b.GetTitle());
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Invalid title: Title must be at least two characters long");
+            }
+
+
+            Console.ReadKey();
         }
     }
 }
