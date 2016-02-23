46d52ce4ca0852cd4fd753c08880b9467abf5df4
Toke Frostholm Rasmussen <toke1011@edu.easj.dk>
2016-02-23 20:40:47 +0100

    Created unit test class, but im stuck here

---
+++ ExceptionsAndUnitTestTests/BookTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExceptionsAndUnitTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionsAndUnitTest.Tests
+{
+    [TestClass()]
+    public class BookTests
+    {
+        [TestMethod()]
+        public void SetTitleTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetTitleTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SetYearTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetYearTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ ExceptionsAndUnitTestTests2/BookTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExceptionsAndUnitTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionsAndUnitTest.Tests
+{
+    [TestClass()]
+    public class BookTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void SetTitleTest()
+        {
+                //throw new ArgumentOutOfRangeException();
+        }
+    }
+}
